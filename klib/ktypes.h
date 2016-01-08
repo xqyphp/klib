e6ee5b01a740c343399d7beb936c99ffc2f45815
likai <xqyphp@github.com>
2016-01-08 23:07:15 +0800

    add list lib

---
+++ klib/ktypes.h
@@ -0,0 +1,21 @@
+//
+//  types.h
+//  klib
+//
+//  Created by LiKai on 16/1/8.
+//  Copyright © 2016年 LiKai. All rights reserved.
+//
+
+#ifndef ktypes_h
+#define ktypes_h
+
+#include <stddef.h>
+
+typedef unsigned            k_bool_t;
+typedef int                       k_status_t;
+
+#define K_TRUE         1
+#define K_FALSE        0
+#define K_NULL          ((void*)0)
+
+#endif /* ktypes_h */
---
+++ klib/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  klib
+//
+//  Created by LiKai on 16/1/8.
+//  Copyright © 2016年 LiKai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
